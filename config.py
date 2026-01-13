be36579ffd7d62a5de4209798e52ac4d922e5124
Marie! <marie-point@hotmail.fr>
2026-01-13 15:38:53 +0100

    config file

---
+++ config.py
@@ -0,0 +1,7 @@
+# Configuration Template
+# modifiez les chemins selon votre environnement local
+
+DATA_PATH = "IRCCyN_IVC_DIBR_Videos/"
+VIDEOS_PATH = "IRCCyN_IVC_DIBR_Videos/Videos/"
+
+# Autres chemins si nécessaire
